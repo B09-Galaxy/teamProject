6fde8a9be96418938a6f7927d65645f01f58da70
muyeon <gmakin36@gmail.com>
2024-07-09 19:41:34 +0900

    init: setup search page

---
+++ src/app/search/page.tsx
@@ -0,0 +1,11 @@
+function SearchPage() {
+  return (
+    <div className="flex justify-center items-center h-screen flex-col">
+      <section>출발지 / 도착지</section>
+      <section>가는날</section>
+      <section>인원</section>
+    </div>
+  );
+}
+
+export default SearchPage;
