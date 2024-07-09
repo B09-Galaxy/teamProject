43dfceb170518d864ea576faed2be605711e6fb1
hb9901 <hb9901@gmail.com>
2024-07-10 00:24:56 +0900

    feat: 버스 & 기차 운행 정보 api 호출 서버 제작 및 버스정류장, 기차역 정보 파일 생성

---
+++ src/app/api/bus/info/route.ts
@@ -0,0 +1,32 @@
+import axios from 'axios';
+import { NextRequest, NextResponse } from 'next/server';
+
+export const GET = async (request: NextRequest) => {
+  const { searchParams } = new URL(request.url);
+  const pageNo = searchParams.get('pageNo');
+  const numOfRows = searchParams.get('numOfRows');
+  const depTerminalId = searchParams.get('depTerminalId');
+  const arrTerminalId = searchParams.get('arrTerminalId');
+  const depPlandTime = searchParams.get('depPlandTime');
+
+  if (!pageNo || !numOfRows || !depTerminalId || !arrTerminalId || !depPlandTime)
+    return NextResponse.json('', { status: 400 });
+  try {
+    const response = await axios.get(`http://apis.data.go.kr/1613000/ExpBusInfoService/getStrtpntAlocFndExpbusInfo`, {
+      params: {
+        serviceKey: process.env.NEXT_PUBLIC_BUS_DECODING_KEY,
+        pageNo,
+        numOfRows,
+        _type: 'json',
+        depTerminalId,
+        arrTerminalId,
+        depPlandTime
+      }
+    });
+    const data = response.data.response.body;
+    console.log(response);
+    return NextResponse.json(data);
+  } catch (error) {
+    return NextResponse.json({ error: 'Failed to fetch data' });
+  }
+};
