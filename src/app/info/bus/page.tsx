fd25adafcc71a3ba9165251b9a32d86af888defb
SecretCandy <parksw9762@gmail.com>
2024-07-12 23:44:16 +0900

    ddfafadsf

--- src/app/info/bus/page.tsx
+++ src/app/info/bus/page.tsx
@@ -6,6 +6,7 @@ import { useSearchParams } from 'next/navigation';
 import { useEffect, useState } from 'react';
 import busStation from '@/assets/busStation.json';
 import Link from 'next/link';
+import { TrainPageLoading } from '@/components/TrainPage/TrainPageLoading';
 
 function BusPage() {
   const searchparams = useSearchParams();
@@ -20,8 +21,12 @@ function BusPage() {
   const depPlandTime = date;
   const depTerminalId = (busStation as BusStationType)[departure];
   const arrTerminalId = (busStation as BusStationType)[arrival];
+  const depYear = date.slice(0, 4);
+  const depMonth = date.slice(4, 6);
+  const depDay = date.slice(6);
 
   const [datas, setDatas] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     const fetchBusApi = async () => {
@@ -32,25 +37,28 @@ function BusPage() {
         arrTerminalId,
         depPlandTime
       });
+      setIsLoading(false);
       setDatas(response.data.items.item);
     };
     fetchBusApi();
   }, []);
+
+  if (isLoading) return <TrainPageLoading />;
 
   return (
     <div className="w-[1000px] mx-auto">
       <div className="flex flex-col m-5 mx-auto gap-2.5">
         <h1 className="text-center text-2xl font-extrabold">승차권 조회</h1>
         <p className="text-center text-sm text-gray-600">
-          {`${departure} → ${arrival} | ${PEOPLE} ${PEOPLE_COUNT} 명 | ${date}`}
+          {departure} → {arrival} | {PEOPLE} {PEOPLE_COUNT}명 | {depYear}년 {depMonth}월 {depDay}일
         </p>
       </div>
-      <div className="w-4/5 mb-2.5 mx-auto flex flex-row justify-center gap-2.5">
-        <button className="w-2/5 h-10 m-1.5 text-xl font-bold rounded-md border border-solid bg-blue-400 text-white">
+      <div className="w-4/5 mb-2.5 mx-auto flex flex-row justify-center">
+        <button className="w-2/5 h-10 m-1.5 text-xl font-bold rounded-md border border-solid bg-[#0076be] text-white">
           버스
         </button>
         <Link
-          className="w-2/5 h-10 m-1.5 pt-1.5 text-center text-xl font-bold rounded-md border border-solid text-gray-600"
+          className="w-2/5 h-10 m-1.5 pt-1.5 text-xl text-center font-bold rounded-md border hover:border-2 border-solid border-gray-300 hover:border-[#0076be] text-gray-600"
           href={`/info/train?departure=${departure}&arrival=${arrival}&date=${date}`}
         >
           열차
